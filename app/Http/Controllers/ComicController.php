be3cb42c99bd4ad53bdcad55452ae0a43a8366be
dunzolo <rossidavide108@gmail.com>
2023-02-20 14:27:02 +0100

    added function index ComicController

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comic;
 
 class ComicController extends Controller
 {
-    //
+    ///metodo che mostra tutti i record
+    public function index(){
+        $comics = Comic::all();
+        //devo indicare il nome della view
+        return view('home', compact('comics'));
+    }
 }
